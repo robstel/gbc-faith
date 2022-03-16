566d0a281aa9930c10c903e3c505c404a110b06f
Rob <rob@robstel.co.uk>
2022-03-16 17:11:22 +0000

    General fixes
    Adds shortcodes [gbc-child-pages] and [gbc-excerpt].
    Style tweaks including header text colour.
    Fix layout and styling for sermons.

--- functions.php
+++ functions.php
@@ -1,25 +1,44 @@
 <?php
 
-// enqueue scripts and style from the parent theme
+// enqueue the parent and child theme styles
 //
-add_action( 'wp_enqueue_scripts', 'gbc_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'gbc_enqueue_styles');
 function gbc_enqueue_styles() {
 
-    $parenthandle = 'faith-style';
+    $parent_handle = 'faith-style';
     $theme = wp_get_theme();
 
-    wp_enqueue_style($parenthandle,
+    wp_enqueue_style($parent_handle,
         get_template_directory_uri() . '/style.css',
         array(),
         $theme->parent()->get('Version')
-    );    
+    );
 
-    wp_enqueue_style( 'gbc-faith-style',
+    wp_enqueue_style('gbc-faith-style',
         get_stylesheet_uri(),
-        array( $parenthandle ), 
+        array( $parent_handle ),
         $theme->get('Version')
     );
 }
 
-add_post_type_support( 'page', 'excerpt' );
+// add shortcode [gbc-child-pages] to list child pages
+//
+function gbc_list_child_pages() {
 
+    global $post;
+
+    $id = (is_page() && $post->post_parent) ? $post->post_parent : $post->ID;
+    $child_pages = wp_list_pages('sort_column=menu_order&title_li=&child_of=' . $id . '&echo=0');
+    if ($child_pages) {
+        return '<ul class="gbc-page-list">' . $child_pages . '</ul>';
+    }
+}
+add_shortcode('gbc-child-pages', 'gbc_list_child_pages');
+
+// add shortcode [gbc-excerpt] to show the excerpt
+///
+add_shortcode('gbc-excerpt', 'get_the_excerpt');
+
+// enable excerpts on pages
+//
+add_post_type_support('page', 'excerpt');
